cef3c390486878ba8c2749f9973207814ce235dd
Maxim <eremenko98@yadnex.ru>
2024-03-23 02:57:37 +0700

    temp Commit

--- server/model/src/API_Grabber.py
+++ server/model/src/API_Grabber.py
@@ -1,3 +1,10 @@
+from copy import deepcopy
+from datetime import datetime, timedelta
+import time
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3 import Retry
+
 from datetime import datetime, timedelta
 import time
 import requests
@@ -6,7 +13,7 @@ from urllib3 import Retry
 
 class API_Grabber():
     def __init__(self, per_page=100):
-        self.base_url = "https://api.hh.ru/vacancies"
+        self.base_url = "https://api.hh.ru"
         self.per_page = per_page
         generator = DateIntervalGenerator()
         self.hour_interval = generator.generate_hour_intervals()
@@ -23,7 +30,7 @@ class API_Grabber():
     def fetch_page(self, url, params):
         ext = "".join(f"&{i}={params[i]}" for i in params)
         url = f"{self.base_url}?{ext}"
-        headers = {"User-Agent": "Mediapartners-Google"}
+        
         session = requests.Session()
 
         retries = Retry(total=3, backoff_factor=1, status_forcelist=[429, 500, 502, 503, 504])
@@ -42,18 +49,20 @@ class API_Grabber():
             print(f"Error: {e}")
 
     def prepare_request(self, date_from, date_to):
+        params = []
         url = f"{self.base_url}?&date_from={date_from}&date_to={date_to}"
         pagination = self.get_pagination(url)
         collected_data = []
         if pagination >= 20:
             pagination = 19
         for page in range(0, pagination):
-            params = {
+            params.append({
                 'date_from': date_from,
                 'date_to': date_to,
                 'per_page': self.per_page,
                 'page': page
-            }
+            })
+            return url, params
             response = self.fetch_page(params)
             if response:
                 collected_data.extend(response.get('items', []))
@@ -67,9 +76,37 @@ class API_Grabber():
         for interval in self.hour_interval:
             if count != -1 and len(response) > count:
                 break
-            response.append(self.get_data_for_date_interval(interval["date_from"], interval["date_to"]))
+            url, params = self.prepare_request(interval["date_from"], interval["date_to"])
+            response.append(self.fetch_page(url, params))
 
         return response
+    
+    def get_area(self):
+        pass
+
+class url_creator:
+    def __init__(self, url, name):
+        self.name = name
+        self.url = url
+        self.header = {"User-Agent": "Mediapartners-Google"}
+
+    def set_header(self, header):
+        self.header = deepcopy(header)
+
+    def get_header(self):
+        return self.header
+
+class Area_url_creator(url_creator):
+    def __init__(url):
+        super().__init__(url, "/area")
+
+    def build_url(self):
+        return f"{self.url}{self.name}"
+
+class Vacancy_url_creator(url_creator):
+    def __init__(url):
+        super().__init__(url, "/vacansies")
+        
         
 class DateIntervalGenerator:
     def __init__(self):
