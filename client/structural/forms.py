2a5b4f39618867165e661db55897e9af4757fcb3
Maxim <eremenko98@yadnex.ru>
2024-05-06 20:07:10 +0700

    com

--- client/structural/forms.py
+++ client/structural/forms.py
@@ -1,11 +1,9 @@
-import socket
 from tkinter import *
 from tkinter import ttk
 from structural.src.request_builder import *
 from structural.src.graphs import *
 from structural.src.request_context import *
 from creational.singleton import Singleton
-from structural import config
 
 class MainForm(Frame):
     def __init__(self, master, token):
@@ -117,32 +115,3 @@ class SearchForm(Frame, Singleton):
         area = self.entry_area.get()
         exp = self.selected_exp.get()
         self.create_new_form(self.on_search(self.token, vacancy_name, area, exp), vacancy_name)
-
-def search(token, vacancy_name, area, exp):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-    try:
-        sock.connect((config.host_ip, config.port))
-        request_builder = JSONRequestBuilder(GetRequestTemplate(token, vacancy_name, area, exp))
-       
-        request = request_builder.build()
-        message = request.encode(config.encoding)
-        sock.send(message)
-        answer = b""
-        end_signal = b"<END>"
-        while True:
-            data = sock.recv(1024)
-            answer += data
-            if end_signal in data:
-                break
-        answer = answer[:-5].decode(config.encoding)
-        answer = json.loads(answer)
-        
-        sock.close()
-        return RequestContext(answer["data"], json.loads(request))
-    except Exception as error:
-        print("Произошла ошибка:", error)
-    except:
-        print("На сервере ведутся технические работы приносим свои извинение за предоставленные неудобства."
-            "\nПопробуйте повторить попытку через пару минут")
-        sock.close()
