9c240b0b0926f77a0979491bb208e29b83b0edce
Maxim <eremenko98@yadnex.ru>
2024-05-07 16:26:27 +0700

    fix histograms

--- client/structural/src/graphs.py
+++ client/structural/src/graphs.py
@@ -31,10 +31,22 @@ class Graph:
     
 class BaseHistogram(Graph):
     def draw(self, container) -> None:
+        salaries = [int(i) for i in self.dp["salary"] if pd.notnull(i)] 
+        q1 = np.quantile(salaries, 0.25)  # 25% квантиль
+        q3 = np.quantile(salaries, 0.75)  # 75% квантиль
+
+        iqr = q3 - q1  # Межквартильный размах
+        # Определение границ для отсечения выбросов
+
+        lower_bound = q1 - 1.5 * iqr
+        upper_bound = q3 + 1.5 * iqr
+        # Фильтрация выбросов
+        filtered_salaries = [x for x in salaries if lower_bound < x < upper_bound]
+
         fig, ax = plt.subplots(figsize=(10, 6))
         fig.subplots_adjust(bottom=0.2) 
-        ax.hist(self.dp["salary"], bins=15, color="skyblue", edgecolor='black')
-        salaries = [int(i) for i in self.dp["salary"] if pd.notnull(i)]
+        ax.hist(filtered_salaries, bins=15, color="skyblue", edgecolor='black')
+        
         mean_salary = np.mean(salaries)
         median_salary = np.median(salaries)
         ax.set_xlabel("Зарплата, Рублей")
@@ -42,7 +54,7 @@ class BaseHistogram(Graph):
         ax.set_title(f"Распределение зарплат для вакансии: {self.context['vacancy_name']}\nСредняя зарплата: {mean_salary:.2f}, Медиана зарплаты: {median_salary:.2f}")
         ax.axvline(mean_salary, color="r", linestyle="dashed", linewidth=1)
         ax.axvline(median_salary, color="g", linestyle="dotted", linewidth=1)
-        sample_size = len(self.dp["salary"].dropna())  # Исключаем NaN значения
+        sample_size = len(filtered_salaries)  # Исключаем NaN значения
  
         fig.text(0.5, 0.02, f'Размер выборки: {sample_size}', ha='center', va='bottom', fontsize=10)
         ax.legend(["Среднее", "Медиана"])
